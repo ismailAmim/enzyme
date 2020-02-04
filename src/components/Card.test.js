0e0c00efb403edf87055c151449944d39e145615
bob <ismail.amim2010@gmail.com>
2020-02-04 12:06:04 +0100

    snapshot testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,6 +3,10 @@ import { shallow, mount, render } from 'enzyme';
 import Card from './Card';
 
 
-it('expect to render Card component ', () => {
+/* it('expect to render Card component ', () => {
     expect(shallow(<Card/>).length).toEqual(1);
-    });
+    }); */
+
+it('expect to render Card component ', () => {
+    expect(shallow(<Card/>)).toMatchSnapshot();
+    })
